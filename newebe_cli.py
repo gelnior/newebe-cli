10d751f69c59797ce8486878091222c77275b19a
Gelnior <gelnior@free.fr>
2013-12-08 18:35:34 +0100

    CLI for newebe

---
+++ newebe_cli.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+import os
+import json
+import requests
+from docopt import docopt
+
+DOC = """Newebe CLI.
+
+Usage:
+  newebe_cli.py get lastmessages [--config=config_file]
+  newebe_cli.py post <message> [--tag=<tag>] [--config=config_file]
+
+  newebe_cli.py (-h | --help)
+  newebe_cli.py --version
+
+Options:
+  -h --help                Show this screen.
+  --version                Show version.
+  --tag=<tag>              Post messaage with given tag.
+  --config=<config_file>   Change config file location
+                           (default: ~/.newebe-config).
+
+"""
+CONFIG_FILE_NAME = os.path.join(os.path.expanduser('~'), '.newebe-config')
+
+
+def get_config():
+    """
+    Get config from config file
+    """
+    with open(CONFIG_FILE_NAME) as config_file:
+        lines = config_file.readlines()
+        if len(lines) > 1:
+            url = lines[0].strip()
+            password = lines[1].strip()
+            url = 'http://%s/' % url
+            return (url, password)
+
+    return (None, None)
+
+
+def login(url, password):
+    """
+    Log the cli so the user can access to his newebe.
+    """
+    requests.post(url + 'login/', {"password": password}, verify=False)
+
+
+def last_messages(url, password):
+    """
+    Return a string containing the last 10 messages of the newebe news feed.
+    """
+    messages = []
+    resp = requests.get(url + 'microposts/all/', verify=False)
+    for message in reversed(resp.json()["rows"]):
+        messages.append("-" * 80)
+        date = message["date"].replace("T", " ").replace("Z", " ")
+        messages.append("%s - %s" % (message["author"], date))
+        messages.append(message["content"])
+    messages.append("-" * 80)
+    return "\n".join(messages)
+
+
+def post_message(message, tag):
+    '''
+    Post a new message to your Newebe.
+    '''
+    path = url + 'microposts/all/'
+    data = {"content": message, "tags": [tag]}
+    resp = requests.post(path, json.dumps(data), verify=False)
+    print resp.text
+
+
+if __name__ == '__main__':
+    arguments = docopt(DOC, version='Newebe CLI 0.1.0')
+
+    if arguments["--config"]:
+        CONFIG_FILE_NAME = arguments["--config"]
+    (url, password) = get_config()
+
+    if arguments["get"] and arguments["lastmessages"]:
+        login(url, password)
+        print last_messages(url, password)
+
+    if arguments["post"]:
+        message = arguments["<message>"]
+        tag = arguments["--tag"]
+        if not tag:
+            tag = "all"
+        post_message(message, tag)
